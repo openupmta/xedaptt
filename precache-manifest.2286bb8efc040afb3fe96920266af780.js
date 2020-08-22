6d2169ef09751af18e3672d673d2676e9804de1f
openupmta <openupmta99@gmail.com>
2020-08-22 20:54:10 +0700

    Updates

---
+++ precache-manifest.2286bb8efc040afb3fe96920266af780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbd390a4cba46c08399c2f2613c7919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27868b9aceefc361f25a",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1787b33829340cdcb86a",
+    "url": "/static/js/2.60723490.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.60723490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27868b9aceefc361f25a",
+    "url": "/static/js/main.d78edb42.chunk.js"
+  },
+  {
+    "revision": "dae948b4ad81586f7e1b",
+    "url": "/static/js/runtime-main.135eeda4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/xedaptt/precache-manifest.ca43c1f2c95ddaf4e39dc4f968393118.js"
+  "/precache-manifest.2286bb8efc040afb3fe96920266af780.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xedaptt/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
